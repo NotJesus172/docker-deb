402a00ef468c28077e63f1d6953a3530d20620ad
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2024-09-19 00:04:43 -0400

    1.0.0

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+#	Update and install nala:
 sudo apt-get update
 sudo apt-get install -y nala
+
+#	 Update and upgrade the system:
 sudo nala update
 sudo nala upgrade -y
+
+#	Install docker engine:
 sudo nala install -y curl ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
@@ -11,6 +17,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#	Install docker packages
 sudo nala update
 sudo nala install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-curl -SL https://github.com/docker/compose/releases/download/v2.29.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+
+#	Install docker-compose:
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+
